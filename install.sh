dcd2ff8185c5f7b0449ebc6cd0869a86da8e2fa8
Sviatoslav Liashenko <sweathrtx@gmail.com>
2024-02-22 19:54:45 +0200

    Added `install.sh\' script.

---
+++ install.sh
@@ -0,0 +1,14 @@
+git clone --bare git@github.com:seijinriashenko/dots.git $HOME/.dots
+function dots {
+   /usr/bin/git --git-dir=$HOME/.dots/ --work-tree=$HOME $@
+}
+dots checkout
+if [ $? = 0 ]; then
+  echo "Checked out dots.";
+  else
+    echo "Backing up pre-existing dotfiles.";
+    mkdir -p .dots-backup
+    dots checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dots-backup/{}
+fi;
+dots checkout
+dots config status.showUntrackedFiles no
